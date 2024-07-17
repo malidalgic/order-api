d67d8e95ce9d1cb1d7aec5786a6c07019f29e47e
Mehmet Ali DALĞIÇ <malidalgic51@gmail.com>
2024-07-17 17:43:33 +0300

    refactoring

--- src/main/java/com/abas/orderapi/controller/OrderController.java
+++ src/main/java/com/abas/orderapi/controller/OrderController.java
@@ -4,9 +4,7 @@ import com.abas.orderapi.entity.Order;
 import com.abas.orderapi.helper.OrderBuilder;
 import com.abas.orderapi.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -17,25 +15,35 @@ import java.util.Map;
 public class OrderController {
 
     private final OrderService orderService;
-    private final List<Order> orders = OrderBuilder.getOrders();
 
     @GetMapping("/total-amount")
     public double getTotalAmount() {
+        List<Order> orders = OrderBuilder.getOrders();
         return orderService.getTotalAmount(orders);
     }
 
     @GetMapping("/average-price")
     public double getAveragePrice() {
+        List<Order> orders = OrderBuilder.getOrders();
         return orderService.getAveragePrice(orders);
     }
 
     @GetMapping("/average-prices-by-item")
     public Map<Integer, Double> getAveragePricesByItem() {
+        List<Order> orders = OrderBuilder.getOrders();
         return orderService.getAveragePricesByItem(orders);
     }
 
     @GetMapping("/item-order-counts")
     public Map<Integer, Map<Integer, Integer>> getItemOrderCounts() {
+        List<Order> orders = OrderBuilder.getOrders();
         return orderService.getItemOrderCounts(orders);
+    }
+
+    @PostMapping("/add")
+    public Order addOrder(@RequestBody Order order) {
+        List<Order> orders = OrderBuilder.getOrders();
+        orders.add(order);
+        return order;
     }
 }
--- src/main/java/com/abas/orderapi/helper/OrderBuilder.java
+++ src/main/java/com/abas/orderapi/helper/OrderBuilder.java
@@ -2,12 +2,14 @@ package com.abas.orderapi.helper;
 
 import com.abas.orderapi.entity.Order;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class OrderBuilder {
     public static List<Order> getOrders() {
 
-        return List.of(
+        return new ArrayList<>(Arrays.asList(
                 Order.builder().orderNumber(1000).itemNumber(2000).quantity(12).unitPrice(100.51).build(),
                 Order.builder().orderNumber(1000).itemNumber(2001).quantity(31).unitPrice(200).build(),
                 Order.builder().orderNumber(1000).itemNumber(2002).quantity(22).unitPrice(150.86).build(),
@@ -24,6 +26,6 @@ public class OrderBuilder {
                 Order.builder().orderNumber(1002).itemNumber(2003).quantity(8).unitPrice(12.1).build(),
                 Order.builder().orderNumber(1002).itemNumber(2002).quantity(16).unitPrice(94).build(),
                 Order.builder().orderNumber(1002).itemNumber(2005).quantity(9).unitPrice(44.1).build(),
-                Order.builder().orderNumber(1002).itemNumber(2006).quantity(19).unitPrice(90).build());
+                Order.builder().orderNumber(1002).itemNumber(2006).quantity(19).unitPrice(90).build()));
     }
 }
