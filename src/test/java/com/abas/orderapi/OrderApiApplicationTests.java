061b813757c89e6b4cb3cc7ee0fe45123f8957b1
Mehmet Ali DALĞIÇ <malidalgic51@gmail.com>
2024-07-17 16:13:11 +0300

    initial

---
+++ src/test/java/com/abas/orderapi/OrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abas.orderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
