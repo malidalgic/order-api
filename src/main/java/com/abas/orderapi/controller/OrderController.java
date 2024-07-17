7d569773a736fd926a26a9d706590c9c80eeb1ba
Mehmet Ali DALĞIÇ <malidalgic51@gmail.com>
2024-07-17 17:47:58 +0300

    refactoring

--- src/main/java/com/abas/orderapi/controller/OrderController.java
+++ src/main/java/com/abas/orderapi/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.abas.orderapi.entity.Order;
 import com.abas.orderapi.helper.OrderBuilder;
 import com.abas.orderapi.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,35 +17,32 @@ import java.util.Map;
 public class OrderController {
 
     private final OrderService orderService;
+    private final List<Order> orders = OrderBuilder.getOrders();
+
 
     @GetMapping("/total-amount")
     public double getTotalAmount() {
-        List<Order> orders = OrderBuilder.getOrders();
         return orderService.getTotalAmount(orders);
     }
 
     @GetMapping("/average-price")
     public double getAveragePrice() {
-        List<Order> orders = OrderBuilder.getOrders();
         return orderService.getAveragePrice(orders);
     }
 
     @GetMapping("/average-prices-by-item")
     public Map<Integer, Double> getAveragePricesByItem() {
-        List<Order> orders = OrderBuilder.getOrders();
         return orderService.getAveragePricesByItem(orders);
     }
 
     @GetMapping("/item-order-counts")
     public Map<Integer, Map<Integer, Integer>> getItemOrderCounts() {
-        List<Order> orders = OrderBuilder.getOrders();
         return orderService.getItemOrderCounts(orders);
     }
 
     @PostMapping("/add")
-    public Order addOrder(@RequestBody Order order) {
-        List<Order> orders = OrderBuilder.getOrders();
+    public ResponseEntity<Order> addOrder(@RequestBody Order order) {
         orders.add(order);
-        return order;
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 }
